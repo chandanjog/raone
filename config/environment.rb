f02ddd73108dc25720797d53bb5ac32a04176a2e
chandan <chandan@chandansj.(none)>
2010-08-23 11:52:23 +0530

    <cj> scss now working

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ RAILS_GEM_VERSION = '2.2.3' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem "haml"
+  config.gem "haml" , :version=>'3.1.0'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
